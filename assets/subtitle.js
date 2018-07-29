a944bc4bc470d2c5eabf8837fb43021fd9a88af1
michaellee8 <ckmichael8@gmail.com>
2018-07-29 16:29:28 +0800

    Add some assets

---
+++ assets/subtitle.js
@@ -0,0 +1,408 @@
+export default [
+  {
+    text: [
+      "The",
+      "snow",
+      "glows",
+      "white",
+      "on",
+      "the",
+      "mountain",
+      "tonight",
+      "Not",
+      "a",
+      "footprint",
+      "to",
+      "be",
+      "seen"
+    ],
+    time: 15259,
+    duration: 7000,
+    originalText:
+      "The snow glows white on the mountain tonight\nNot a footprint to be seen"
+  },
+  {
+    text: [
+      "A",
+      "kingdom",
+      "of",
+      "isolation",
+      "And",
+      "it",
+      "looks",
+      "like",
+      "I'm",
+      "the",
+      "queen"
+    ],
+    time: 22259,
+    duration: 5741,
+    originalText: "A kingdom of isolation\nAnd it looks like I'm the queen"
+  },
+  {
+    text: [
+      "The",
+      "wind",
+      "is",
+      "howling",
+      "like",
+      "this",
+      "swirling",
+      "storm",
+      "inside"
+    ],
+    time: 29200,
+    duration: 5680,
+    originalText: "The wind is howling like this swirling storm\ninside"
+  },
+  {
+    text: [
+      "Couldn't",
+      "keep",
+      "it",
+      "in,",
+      "",
+      "heaven",
+      "knows",
+      "I've",
+      "tried"
+    ],
+    time: 36000,
+    duration: 6000,
+    originalText: "Couldn't keep it in, \nheaven knows I've tried"
+  },
+  {
+    text: ["Don't", "let", "them", "in,", "", "don't", "let", "them", "see"],
+    time: 43000,
+    duration: 3500,
+    originalText: "Don't let them in, \ndon't let them see"
+  },
+  {
+    text: ["Be", "the", "good", "girl", "you", "always", "have", "to", "be"],
+    time: 47000,
+    duration: 3000,
+    originalText: "Be the good girl you always have to be"
+  },
+  {
+    text: [
+      "Conceal,",
+      "don't",
+      "feel,",
+      "don't",
+      "let",
+      "them",
+      "know",
+      "Well,",
+      "now",
+      "they",
+      "know"
+    ],
+    time: 50000,
+    duration: 9000,
+    originalText:
+      "Conceal, don't feel, don't let them know\nWell, now they know"
+  },
+  {
+    text: [
+      "Let",
+      "it",
+      "go,",
+      "let",
+      "it",
+      "go",
+      "Can't",
+      "hold",
+      "it",
+      "back",
+      "anymore"
+    ],
+    time: 60000,
+    duration: 7000,
+    originalText: "Let it go, let it go\nCan't hold it back anymore"
+  },
+  {
+    text: [
+      "Let",
+      "it",
+      "go,",
+      "let",
+      "it",
+      "go",
+      "Turn",
+      "away",
+      "and",
+      "slam",
+      "the",
+      "door"
+    ],
+    time: 67000,
+    duration: 7000,
+    originalText: "Let it go, let it go\nTurn away and slam the door"
+  },
+  {
+    text: [
+      "I",
+      "don't",
+      "care",
+      "what",
+      "they're",
+      "going",
+      "to",
+      "say",
+      "Let",
+      "the",
+      "storm",
+      "rage",
+      "on"
+    ],
+    time: 74600,
+    duration: 9500,
+    originalText:
+      "I don't care what they're going to say\nLet the storm rage on"
+  },
+  {
+    text: ["The", "cold", "never", "bothered", "me", "anyway"],
+    time: 85000,
+    duration: 3000,
+    originalText: "The cold never bothered me anyway"
+  },
+  {
+    text: [
+      "It's",
+      "funny",
+      "how",
+      "some",
+      "distance",
+      "makes",
+      "everything",
+      "seem",
+      "small"
+    ],
+    time: 92000,
+    duration: 6000,
+    originalText: "It's funny how some distance makes everything seem small"
+  },
+  {
+    text: [
+      "And",
+      "the",
+      "fears",
+      "that",
+      "once",
+      "controlled",
+      "me,",
+      "",
+      "can't",
+      "get",
+      "to",
+      "me",
+      "at",
+      "all"
+    ],
+    time: 98500,
+    duration: 7500,
+    originalText:
+      "And the fears that once controlled me, \ncan't get to me at all"
+  },
+  {
+    text: ["It's", "time", "to", "see", "what", "I", "can", "do"],
+    time: 106000,
+    duration: 4000,
+    originalText: "It's time to see what I can do"
+  },
+  {
+    text: ["To", "test", "the", "limits", "and", "break", "through"],
+    time: 110000,
+    duration: 3500,
+    originalText: "To test the limits and break through"
+  },
+  {
+    text: [
+      "No",
+      "right,",
+      "no",
+      "wrong,",
+      "no",
+      "rules",
+      "for",
+      "me",
+      "I'm",
+      "free"
+    ],
+    time: 113500,
+    duration: 8000,
+    originalText: "No right, no wrong, no rules for me\nI'm free"
+  },
+  {
+    text: [
+      "Let",
+      "it",
+      "go,",
+      "let",
+      "it",
+      "go",
+      "I",
+      "am",
+      "one",
+      "with",
+      "the",
+      "wind",
+      "and",
+      "sky"
+    ],
+    time: 121600,
+    duration: 7000,
+    originalText: "Let it go, let it go\nI am one with the wind and sky"
+  },
+  {
+    text: [
+      "Let",
+      "it",
+      "go,",
+      "let",
+      "it",
+      "go",
+      "You'll",
+      "never",
+      "see",
+      "me",
+      "cry"
+    ],
+    time: 128600,
+    duration: 7400,
+    originalText: "Let it go, let it go\nYou'll never see me cry"
+  },
+  {
+    text: ["Here", "I", "stand", "and", "here", "I'll", "stay"],
+    time: 136000,
+    duration: 6000,
+    originalText: "Here I stand and here I'll stay"
+  },
+  {
+    text: ["Let", "the", "storm", "rage", "on"],
+    time: 142000,
+    duration: 5000,
+    originalText: "Let the storm rage on"
+  },
+  {
+    text: [
+      "My",
+      "power",
+      "flurries",
+      "through",
+      "the",
+      "air",
+      "into",
+      "the",
+      "ground"
+    ],
+    time: 153500,
+    duration: 7500,
+    originalText: "My power flurries through the air into the ground"
+  },
+  {
+    text: [
+      "My",
+      "soul",
+      "is",
+      "spiraling",
+      "in",
+      "frozen",
+      "fractals",
+      "all",
+      "around"
+    ],
+    time: 161000,
+    duration: 7000,
+    originalText: "My soul is spiraling in frozen fractals all around"
+  },
+  {
+    text: [
+      "And",
+      "one",
+      "thought",
+      "crystallizes",
+      "like",
+      "an",
+      "icy",
+      "blast"
+    ],
+    time: 168000,
+    duration: 6000,
+    originalText: "And one thought crystallizes like an icy blast"
+  },
+  {
+    text: [
+      "I'm",
+      "never",
+      "going",
+      "back,",
+      "the",
+      "past",
+      "is",
+      "in",
+      "the",
+      "past"
+    ],
+    time: 174000,
+    duration: 8000,
+    originalText: "I'm never going back, the past is in the past"
+  },
+  {
+    text: [
+      "Let",
+      "it",
+      "go,",
+      "let",
+      "it",
+      "go",
+      "And",
+      "I'll",
+      "rise",
+      "like",
+      "the",
+      "break",
+      "of",
+      "dawn"
+    ],
+    time: 183000,
+    duration: 7000,
+    originalText: "Let it go, let it go\nAnd I'll rise like the break of dawn"
+  },
+  {
+    text: [
+      "Let",
+      "it",
+      "go,",
+      "let",
+      "it",
+      "go",
+      "That",
+      "perfect",
+      "girl",
+      "is",
+      "gone"
+    ],
+    time: 190000,
+    duration: 7500,
+    originalText: "Let it go, let it go\nThat perfect girl is gone"
+  },
+  {
+    text: ["Here", "I", "stand", "in", "the", "light", "of", "day"],
+    time: 197500,
+    duration: 7500,
+    originalText: "Here I stand in the light of day"
+  },
+  {
+    text: ["Let", "the", "storm", "rage", "on"],
+    time: 205000,
+    duration: 6000,
+    originalText: "Let the storm rage on"
+  },
+  {
+    text: ["The", "cold", "never", "bothered", "me", "anyway"],
+    time: 211000,
+    duration: 4000,
+    originalText: "The cold never bothered me anyway"
+  }
+];
